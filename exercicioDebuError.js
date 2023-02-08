7b21b0005c73db3a284301106a98b577cd149164
CanutoIX <caanutto@gmail.com>
2023-02-08 17:46:36 -0300

    Exercicio-debug-error

--- exercicioDebuError.js
+++ exercicioDebuError.js
@@ -1,9 +1,30 @@
 function validaArray(arr, num) {
-    if(!arr && !num) throw new ReferenceError("Envie os parâmetos");
+    try{
+        if(!arr && !num) throw new ReferenceError("Envie os parâmetos");
 
-    if(typeof arr !== 'object') throw new TypeError("Array precisa ser do tipo object");
+        if(typeof arr !== 'object') throw new TypeError("Array precisa ser do tipo object");
 
-    if(typeof num !== 'number') throw new TypeError("Array precisa ser do tipo number");
+        if(typeof num !== 'number') throw new TypeError("Array precisa ser do tipo number");
 
-    if(arr.lenght !== num) throw new RangeError("Tamanho inválido!");
+        if(arr.lenght !== num) throw new RangeError("Tamanho inválido!");
+
+     return arr;
+    }
+    catch(e) {
+        if(e instanceof ReferenceError) {
+            console.log("ReferenceError!")
+            console.log(e.message)
+        } else if (e instanceof TypeError) {
+            console.log("TypeError!")
+            console.log(e.message)
+    } else if (e instanceof ReferenceError) {{
+        console.log("ReferenceError!")
+        console.log(e.message)
+    }
+    }else {
+        console.log("Erro não esperado:" + e)
+    }
+    }
 }
+
+console.log(validaArray());
